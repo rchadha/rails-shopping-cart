91277d368ec43d400d88d842d1960eb5d3050798
Rishab Chadha <chadha.rishab@gmail.com>
2015-04-26 18:38:05 -0400

    added store controller

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes are not empty" do
     product = Product.new
     assert product.invalid?
@@ -27,7 +29,15 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
 
+  end
 
+  test "product is not valid without a unique title" do
+    product = Product.new(:title => products(:ruby).title,
+                          :description => "yyy",
+                          :price => 1,
+                          :image_url => "fred.gif")
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join(';')
   end
 
   test "the truth" do
