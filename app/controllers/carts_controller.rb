612e10cdc455a8518554e7629289a777a0b105aa
Rishab Chadha <chadha.rishab@gmail.com>
2015-05-14 13:04:13 -0400

    empty shopping cart added

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -55,8 +55,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1.json
   def destroy
     @cart.destroy
+    session[:cart_id] = nil
+
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
