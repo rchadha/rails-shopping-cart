7d082ed1cbc3f135debae982e4c5845271ca0ea1
Rishab Chadha <chadha.rishab@gmail.com>
2015-05-14 14:41:36 -0400

    added cart calculation

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,4 @@
+
 class LineItemsController < ApplicationController
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
@@ -30,7 +31,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, notice: "#{product.title} added to cart :)" }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
